4bb7018b971d285e17904ad5fba6bd6d96b09e43
hbybyyang <2451759073@qq.com>
2024-07-11 01:56:50 +0800

    更新依赖

--- src/interface/interface-list.ts
+++ src/interface/interface-list.ts
@@ -3,4 +3,7 @@ import { 任意接口 } from '@lsby/net-core'
 import * as add_index from '../../src/interface/add/index'
 import * as sub_index from '../../src/interface/sub/index'
 
-export var interfaceList: 任意接口[] = [add_index.default,sub_index.default]
+export var interfaceList: 任意接口[] = [
+  add_index.default,
+  sub_index.default
+]
