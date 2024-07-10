a719b1b7156c668405b28e037172aa350877e867
hbybyyang <2451759073@qq.com>
2024-07-11 00:18:54 +0800

    读取环境变量中的启动端口

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 import { 服务器 } from '@lsby/net-core'
 import { Task } from '@lsby/ts-fp-data'
-import { GlobalLog } from '../global/global'
+import { GlobalEnv, GlobalLog } from '../global/global'
 import add from '../interface/add'
 import sub from '../interface/sub'
 
 export var app = new Task(async () => {
   var log = (await GlobalLog.getInstance().run()).extend('service')
+  var env = await GlobalEnv.getInstance().run()
 
-  const 服务器地址 = (await new 服务器([add, sub], 3000).run().run()).ip
+  const 服务器地址 = (await new 服务器([add, sub], env.APP_PORT).run().run()).ip
   await log.debug('服务器地址: %O', 服务器地址).run()
 })
