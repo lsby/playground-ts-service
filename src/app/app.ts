d68ac9f57379bd04fec0b75eb946b8319d021252
hbybyyang <2451759073@qq.com>
2024-07-11 01:48:32 +0800

    增加生成接口列表的命令

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,12 @@
 import { 服务器 } from '@lsby/net-core'
 import { Task } from '@lsby/ts-fp-data'
 import { GlobalEnv, GlobalLog } from '../global/global'
-import add from '../interface/add'
-import sub from '../interface/sub'
+import { interfaceList } from '../interface/interface-list'
 
 export var app = new Task(async () => {
   var log = (await GlobalLog.getInstance().run()).extend('service')
   var env = await GlobalEnv.getInstance().run()
 
-  const 服务器地址 = (await new 服务器([add, sub], env.APP_PORT).run().run()).ip
+  const 服务器地址 = (await new 服务器(interfaceList, env.APP_PORT).run().run()).ip
   await log.debug('服务器地址: %O', 服务器地址).run()
 })
