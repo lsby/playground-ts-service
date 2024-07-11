316f617b57eeaea15e0190cb6bb57f782c53d749
hbybyyang <2451759073@qq.com>
2024-07-12 07:56:36 +0800

    整理

--- src/interface/user/login/index.ts
+++ src/interface/user/login/index.ts
@@ -1,27 +1,22 @@
 import { 接口, 正确JSON结果, 错误JSON结果 } from '@lsby/net-core'
 import { Task } from '@lsby/ts-fp-data'
-import { GlobalJWT, GlobalKysely, GlobalLog } from '../../../global/global'
+import { GlobalJWT, GlobalLog } from '../../../global/global'
+import { 用户模型 } from '../../../model/user'
 import 接口类型 from './type'
 
 export default new 接口(接口类型, (插件结果) => {
   return new Task(async () => {
     var log = (await GlobalLog.getInstance().run()).extend('login')
 
-    var db = (await GlobalKysely.getInstance().run()).获得句柄()
+    var 用户 = (await 用户模型.通过名称读取用户(插件结果.body.name).run()).getJustOrNull()
 
-    var 存在判定 = await db
-      .selectFrom('user')
-      .select('id')
-      .where('name', '=', 插件结果.body.name)
-      .where('pwd', '=', 插件结果.body.pwd)
-      .executeTakeFirst()
-    if (存在判定 == null) {
+    if (用户 == null || 用户.getPwd() != 插件结果.body.pwd) {
       await log.err('用户名或密码错误').run()
       return new 错误JSON结果('用户名或密码错误' as '用户名或密码错误')
     }
 
     var jwt = await GlobalJWT.getInstance().run()
-    var token = jwt.签名(存在判定.id)
+    var token = jwt.签名(用户.getId())
 
     return new 正确JSON结果({ token })
   })
--- src/model/user.ts
+++ src/model/user.ts
@@ -2,28 +2,28 @@ import { randomUUID } from 'crypto'
 import { Just, Maybe, Nothing, Task } from '@lsby/ts-fp-data'
 import { GlobalKysely } from '../global/global'
 
-export class 用户 {
-  static 通过id读取用户(id: string): Task<Maybe<用户>> {
+export class 用户模型 {
+  static 通过id读取用户(id: string): Task<Maybe<用户模型>> {
     return new Task(async () => {
       var db = (await GlobalKysely.getInstance().run()).获得句柄()
 
       var user = await db.selectFrom('user').select(['id', 'name', 'pwd']).where('id', '=', id).executeTakeFirst()
       if (user == null) return new Nothing()
 
-      return new Just(new 用户({ id: user.id, name: user.name, pwd: user.pwd }))
+      return new Just(new 用户模型({ id: user.id, name: user.name, pwd: user.pwd }))
     })
   }
-  static 通过名称读取用户(name: string): Task<Maybe<用户>> {
+  static 通过名称读取用户(name: string): Task<Maybe<用户模型>> {
     return new Task(async () => {
       var db = (await GlobalKysely.getInstance().run()).获得句柄()
 
       var user = await db.selectFrom('user').select(['id', 'name', 'pwd']).where('name', '=', name).executeTakeFirst()
       if (user == null) return new Nothing()
 
-      return new Just(new 用户({ id: user.id, name: user.name, pwd: user.pwd }))
+      return new Just(new 用户模型({ id: user.id, name: user.name, pwd: user.pwd }))
     })
   }
-  static 创建用户(opt: { name: string; pwd: string }): Task<用户> {
+  static 创建用户(opt: { name: string; pwd: string }): Task<用户模型> {
     return new Task(async () => {
       var db = (await GlobalKysely.getInstance().run()).获得句柄()
 
@@ -33,9 +33,16 @@ export class 用户 {
         .values({ id, ...opt })
         .execute()
 
-      return new 用户({ id, ...opt })
+      return new 用户模型({ id, ...opt })
     })
   }
 
   private constructor(private opt: { id: string; name: string; pwd: string }) {}
+
+  getId(): typeof this.opt.id {
+    return this.opt.id
+  }
+  getPwd(): typeof this.opt.pwd {
+    return this.opt.pwd
+  }
 }
