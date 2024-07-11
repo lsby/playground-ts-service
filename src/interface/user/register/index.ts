6ad41bb98afe1728c699270ae62acf601e1ced3d
hbybyyang <2451759073@qq.com>
2024-07-12 07:30:52 +0800

    整理

--- src/interface/user/register/index.ts
+++ src/interface/user/register/index.ts
@@ -1,37 +1,20 @@
-import { randomUUID } from 'crypto'
 import { 接口, 正确JSON结果, 错误JSON结果 } from '@lsby/net-core'
 import { Task } from '@lsby/ts-fp-data'
-import { GlobalKysely, GlobalLog } from '../../../global/global'
+import { GlobalLog } from '../../../global/global'
+import { 用户 } from '../../../model/user'
 import 接口类型 from './type'
 
 export default new 接口(接口类型, (插件结果) => {
   return new Task(async () => {
     var log = (await GlobalLog.getInstance().run()).extend('login')
 
-    var kysely = await GlobalKysely.getInstance().run()
-    var db = kysely.获得句柄()
-
-    var 存在判定 = await db.selectFrom('user').select('id').where('name', '=', 插件结果.body.name).executeTakeFirst()
-    if (存在判定 != null) {
-      await log.err('用户名已存在').run()
+    var 存在判定 = await 用户.通过名称读取用户(插件结果.body.name).run()
+    if (存在判定.isJust()) {
+      await log.err('用户名已存在: %o', 插件结果.body.name).run()
       return new 错误JSON结果('用户名已存在' as '用户名已存在')
     }
 
-    await kysely
-      .执行事务(
-        (trx) =>
-          new Task(async () => {
-            return trx
-              .insertInto('user')
-              .values({
-                id: randomUUID(),
-                name: 插件结果.body.name,
-                pwd: 插件结果.body.pwd,
-              })
-              .execute()
-          }),
-      )
-      .run()
+    await 用户.创建用户({ name: 插件结果.body.name, pwd: 插件结果.body.pwd }).run()
 
     return new 正确JSON结果({})
   })
---
+++ src/model/user.ts
@@ -0,0 +1,41 @@
+import { randomUUID } from 'crypto'
+import { Just, Maybe, Nothing, Task } from '@lsby/ts-fp-data'
+import { GlobalKysely } from '../global/global'
+
+export class 用户 {
+  static 通过id读取用户(id: string): Task<Maybe<用户>> {
+    return new Task(async () => {
+      var db = (await GlobalKysely.getInstance().run()).获得句柄()
+
+      var user = await db.selectFrom('user').select(['id', 'name', 'pwd']).where('id', '=', id).executeTakeFirst()
+      if (user == null) return new Nothing()
+
+      return new Just(new 用户({ id: user.id, name: user.name, pwd: user.pwd }))
+    })
+  }
+  static 通过名称读取用户(name: string): Task<Maybe<用户>> {
+    return new Task(async () => {
+      var db = (await GlobalKysely.getInstance().run()).获得句柄()
+
+      var user = await db.selectFrom('user').select(['id', 'name', 'pwd']).where('name', '=', name).executeTakeFirst()
+      if (user == null) return new Nothing()
+
+      return new Just(new 用户({ id: user.id, name: user.name, pwd: user.pwd }))
+    })
+  }
+  static 创建用户(opt: { name: string; pwd: string }): Task<string> {
+    return new Task(async () => {
+      var db = (await GlobalKysely.getInstance().run()).获得句柄()
+
+      var id = randomUUID()
+      await db
+        .insertInto('user')
+        .values({ id, ...opt })
+        .execute()
+
+      return id
+    })
+  }
+
+  private constructor(private opt: { id: string; name: string; pwd: string }) {}
+}
