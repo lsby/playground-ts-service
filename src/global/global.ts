ce29f4f3169526fe36de19a16a3e0f82a12a220b
hbybyyang <2451759073@qq.com>
2024-07-14 03:40:01 +0800

    整理

--- src/global/global.ts
+++ src/global/global.ts
@@ -6,31 +6,32 @@ import { GlobalAsyncItem, GlobalItem, GlobalService } from '@lsby/ts-global'
 import { Log } from '@lsby/ts-log'
 import { CronService } from '../tool/common/cron'
 import { Kysely管理器 } from '../tool/common/kysely'
+import { JWT插件 } from '../tool/plugin/jwt'
 import { DB } from '../types/db'
 
-var env = new GlobalItem(
-  'env',
-  new Env({
-    模式: '通过环境变量指定文件路径',
-    环境变量名称: 'ENV_FILE_PATH',
-    环境描述: z.object({
-      APP_PORT: z.coerce.number(),
-      DEBUG_NAME: z.string(),
-      DATABASE_PATH: z.string(),
-      JWT_SECRET: z.string(),
-      JWT_EXPIRES_IN: z.string(),
-      UPLOAD_MAX_FILE_SIZE: z.coerce.number(),
-    }),
+var env = new Env({
+  模式: '通过环境变量指定文件路径',
+  环境变量名称: 'ENV_FILE_PATH',
+  环境描述: z.object({
+    APP_PORT: z.coerce.number(),
+    DEBUG_NAME: z.string(),
+    DATABASE_PATH: z.string(),
+    JWT_SECRET: z.string(),
+    JWT_EXPIRES_IN: z.string(),
+    UPLOAD_MAX_FILE_SIZE: z.coerce.number(),
   }),
-)
+})
+
 export var Global = new GlobalService([
   new GlobalItem('log', new Log('playground-service')),
-  env,
+  new GlobalItem('env', env),
   new GlobalItem('cron', new CronService()),
   new GlobalAsyncItem('kysely', () =>
     env
-      .getValue()
       .获得环境变量()
       .then((env) => new Kysely管理器<DB>(new SqliteDialect({ database: new SQLite(env.DATABASE_PATH) }))),
+  ),
+  new GlobalAsyncItem('jwt-plugin', () =>
+    env.获得环境变量().then((env) => new JWT插件(env.JWT_SECRET, env.JWT_EXPIRES_IN)),
   ),
 ])
--- src/tool/plugin/jwt.ts
+++ src/tool/plugin/jwt.ts
@@ -31,23 +31,11 @@ class JWT签名插件 extends 插件<typeof 签名器类型描述> {
 }
 
 export class JWT插件 {
-  private static instance: JWT插件 | null = null
-  public static async getInstance(密钥: string, 过期时间: string): Promise<JWT插件> {
-    if (!JWT插件.instance) JWT插件.instance = new JWT插件(密钥, 过期时间)
-    if (JWT插件.instance.密钥 != 密钥 || JWT插件.instance.过期时间 != 过期时间) {
-      throw new Error('多次调用JWT插件时参数必须一致')
-    }
-    return JWT插件.instance
-  }
-
   private jwt实例: JWT管理器<JWT负载>
   public 解析器: JWT解析插件
   public 签名器: JWT签名插件
 
-  private constructor(
-    private 密钥: string,
-    private 过期时间: string,
-  ) {
+  constructor(密钥: string, 过期时间: string) {
     this.jwt实例 = new JWT管理器(密钥, 过期时间)
     this.解析器 = new JWT解析插件(this.jwt实例)
     this.签名器 = new JWT签名插件(this.jwt实例)
