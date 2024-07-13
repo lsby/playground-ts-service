af96af82545e718d0d7fca93d849a283c136a191
hbybyyang <2451759073@qq.com>
2024-07-14 05:11:52 +0800

    使用封装的cron

--- src/cron/on-time-alarm.ts
+++ src/cron/on-time-alarm.ts
@@ -1,5 +1,5 @@
+import { Cron } from '@lsby/ts-cron'
 import { Global } from '../global/global'
-import { Cron } from '../tool/common/cron'
 
 export const onTimeAlarm = new Cron('整点报时', '0 0 * * * *', async () => {
   var log = (await Global.getItem('log')).extend('整点报时')
