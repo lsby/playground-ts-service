a1a20d644a6b0685ed73a0e3ff12cd631067ce6d
hbybyyang <2451759073@qq.com>
2024-07-14 04:16:38 +0800

    更新依赖

--- src/util/test/type.ts
+++ src/util/test/type.ts
@@ -1,14 +1,13 @@
 import type { z } from 'zod'
-import { 合并插件结果, 接口类型, 插件 } from '@lsby/net-core'
+import { 取插件内部类型, 合并插件结果, 接口类型, 插件 } from '@lsby/net-core'
 
-type 取值<A> = A extends 插件<infer x> ? x : never
 type 合并JSON插件结果<Arr extends Array<插件<z.AnyZodObject>>> = Arr extends []
   ? {}
   : Arr extends [infer x, ...infer xs]
     ? x extends infer obj
       ? xs extends Array<插件<z.AnyZodObject>>
-        ? 'body' extends keyof z.infer<取值<obj>>
-          ? z.infer<取值<obj>>['body'] & 合并插件结果<xs>
+        ? 'body' extends keyof z.infer<取插件内部类型<obj>>
+          ? z.infer<取插件内部类型<obj>>['body'] & 合并插件结果<xs>
           : {}
         : {}
       : {}
