2bc9d34b9a4cfe631d9410240bb91352b71e22e0
hbybyyang <2451759073@qq.com>
2024-07-20 11:24:25 +0800

    更新依赖

--- src/cron/on-time-alarm.ts
+++ src/cron/on-time-alarm.ts
@@ -1,7 +1,11 @@
 import { Cron } from '@lsby/ts-cron'
 import { Global } from '../global/global'
 
-export var onTimeAlarm = new Cron('整点报时', '0 0 * * * *', async () => {
-  var log = (await Global.getItem('log')).extend('整点报时')
-  await log.debug('我的天啊你看看都%o点了', new Date().getHours())
-})
+export var onTimeAlarm = new Cron(
+  '整点报时',
+  async () => '0 0 * * * *',
+  async () => {
+    var log = (await Global.getItem('log')).extend('整点报时')
+    await log.debug('我的天啊你看看都%o点了', new Date().getHours())
+  },
+)
