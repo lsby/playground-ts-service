c124693b32a691537539358075ebeb1ff7596292
hbybyyang <2451759073@qq.com>
2024-09-06 04:40:27 +0800

    删除模型

--- src/interface/user/login/index.ts
+++ src/interface/user/login/index.ts
@@ -1,19 +1,23 @@
 import { 包装的正确JSON结果, 包装的错误JSON结果, 接口 } from '@lsby/net-core'
 import { Global } from '../../../global/global'
-import { 用户 } from '../../../model/user'
 import 接口类型 from './type'
 
 export default new 接口(接口类型, async (ctx) => {
   var log = (await Global.getItem('log')).extend('login')
+  var db = (await Global.getItem('kysely')).获得句柄()
 
-  var userM = (await 用户.通过名称读取用户基本信息(ctx.body.name)).getJustOrNull()
+  var user = await db
+    .selectFrom('user')
+    .select(['id', 'name', 'pwd'])
+    .where('name', '=', ctx.body.name)
+    .executeTakeFirst()
 
-  if (userM == null || userM.pwd != ctx.body.pwd) {
+  if (user == null || user.pwd != ctx.body.pwd) {
     await log.err('用户名或密码错误')
     return new 包装的错误JSON结果('用户名或密码错误' as const)
   }
 
-  var token = ctx.signJwt({ userId: userM.id })
+  var token = ctx.signJwt({ userId: user.id })
 
   return new 包装的正确JSON结果({ token })
 })
--- src/interface/user/register/index.ts
+++ src/interface/user/register/index.ts
@@ -1,18 +1,19 @@
+import { randomUUID } from 'crypto'
 import { 包装的正确JSON结果, 包装的错误JSON结果, 接口 } from '@lsby/net-core'
 import { Global } from '../../../global/global'
-import { 用户 } from '../../../model/user'
 import 接口类型 from './type'
 
 export default new 接口(接口类型, async (ctx) => {
   var log = (await Global.getItem('log')).extend('login')
+  var db = (await Global.getItem('kysely')).获得句柄()
 
-  var 存在判定 = await 用户.通过名称读取用户(ctx.body.name)
-  if (存在判定.getJustOrNull() != null) {
+  var user = await db.selectFrom('user').select('id').where('name', '=', ctx.body.name).executeTakeFirst()
+  if (user != null) {
     await log.err('用户名已存在: %o', ctx.body.name)
     return new 包装的错误JSON结果('用户名已存在' as const)
   }
 
-  await 用户.不安全的创建用户(ctx.body.name, ctx.body.pwd)
+  await db.insertInto('user').values({ id: randomUUID(), name: ctx.body.name, pwd: ctx.body.pwd }).execute()
 
   return new 包装的正确JSON结果({
     state: 'success' as const,
