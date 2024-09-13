459f1a7e3889b052dbcc27e795ccb76048944b9f
hbybyyang <2451759073@qq.com>
2024-09-13 10:09:56 +0800

    更新依赖

--- src/global/global.ts
+++ src/global/global.ts
@@ -2,12 +2,12 @@ import SQLite from 'better-sqlite3'
 import { SqliteDialect } from 'kysely'
 import { z } from 'zod'
 import { JWT插件 } from '@lsby/net-core-jwt'
+import { Kysely插件 } from '@lsby/net-core-kysely'
 import { CronService } from '@lsby/ts-cron'
 import { Env } from '@lsby/ts-env'
 import { GlobalAsyncItem, GlobalItem, GlobalService } from '@lsby/ts-global'
 import { Kysely管理器 } from '@lsby/ts-kysely'
 import { Log } from '@lsby/ts-log'
-import { Kysely插件 } from '../model/plugin/kysely'
 import { DB } from '../types/db'
 
 var env = new Env({
