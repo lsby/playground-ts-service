9d6948b55bdc11ea6b29c3e7ee4eb5937045fa7c
hbybyyang <2451759073@qq.com>
2024-09-24 12:05:30 +0800

    整理

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,26 @@
 module.exports = {
-  extends: ['@lsby/eslint-config', 'plugin:react/recommended'],
   settings: {
-    react: {
-      version: 'detect',
-    },
+    react: { version: 'detect' },
   },
+  extends: [
+    // 主配置
+    '@lsby/eslint-config',
+    // hook规则
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['react'],
   rules: {
-    'react/jsx-pascal-case': 'off',
+    // 迭代元素必须有key
+    'react/jsx-key': 'error',
+    // 不可以出现重复的属性
+    'react/jsx-no-duplicate-props': 'error',
+    // 不可以用数组的索引做key, 因为react是依据位置决定是否重新渲染的
+    'react/no-array-index-key': 'error',
+    // 不可以在jsx中出现没有转换的html实体
+    'react/no-unescaped-entities': 'error',
+    // 不可以使用非预定的属性
+    'react/no-unknown-property': 'error',
+    // 不应该多传props
+    'react/prefer-exact-props': 'error',
   },
 }
