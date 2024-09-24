3fb46117ec06095228b903acedc546c154e4c31c
hbybyyang <2451759073@qq.com>
2024-09-24 18:14:37 +0800

    整理

--- src/types/interface-type.ts
+++ src/types/interface-type.ts
@@ -1,8 +1,8 @@
 export type InterfaceType = [{ path: "/api/base/add"; method: "post"; input: { a: number; b: number; }; successOutput: { status: "success"; data: { res: number; }; }; errorOutput: { status: "fail"; data: never; }; },{ path: "/api/base/sub"; method: "post"; input: { a: number; b: number; }; successOutput: { status: "success"; data: { res: number; }; }; errorOutput: { status: "fail"; data: "未登录"; }; },{ path: "/api/base/upload-file"; method: "post"; input: {}; successOutput: { status: "success"; data: {}; }; errorOutput: { status: "fail"; data: "未登录"; }; },{ path: "/api/user/is-login"; method: "post"; input: {}; successOutput: { status: "success"; data: { isLogin: boolean; }; }; errorOutput: { status: "fail"; data: never; }; },{ path: "/api/user/login"; method: "post"; input: { name: string; pwd: string; }; successOutput: { status: "success"; data: { token: string; }; }; errorOutput: { status: "fail"; data: "用户不存在" | "密码错误"; }; },{ path: "/api/user/register"; method: "post"; input: { name: string; pwd: string; }; successOutput: { status: "success"; data: {}; }; errorOutput: { status: "fail"; data: "用户名已存在"; }; }]
 
-type 元组转联合<T> = T extends any[] ? T[number] : never
+export type 元组转联合<T> = T extends any[] ? T[number] : never
 
-type 所有接口路径们<A = InterfaceType> = A extends []
+export type 所有接口路径们<A = InterfaceType> = A extends []
   ? []
   : A extends [infer x, ...infer xs]
     ? 'path' extends keyof x
@@ -15,25 +15,25 @@ type Get接口路径们<A = InterfaceType> = A extends []
     ? 'method' extends keyof x
       ? x['method'] extends 'get'
         ? 'path' extends keyof x
-          ? [x['path'], ...所有接口路径们<xs>]
+          ? [x['path'], ...Get接口路径们<xs>]
           : never
         : never
       : never
     : never
-type Post接口路径们<A = InterfaceType> = A extends []
+export type Post接口路径们<A = InterfaceType> = A extends []
   ? []
   : A extends [infer x, ...infer xs]
     ? 'method' extends keyof x
       ? x['method'] extends 'post'
         ? 'path' extends keyof x
-          ? [x['path'], ...所有接口路径们<xs>]
+          ? [x['path'], ...Post接口路径们<xs>]
           : never
         : never
       : never
     : never
 
-type 从路径获得参数<Path, A = InterfaceType> = A extends []
-  ? []
+export type 从路径获得参数<Path, A = InterfaceType> = A extends []
+  ? never
   : A extends [infer x, ...infer xs]
     ? 'path' extends keyof x
       ? x['path'] extends Path
@@ -43,8 +43,8 @@ type 从路径获得参数<Path, A = InterfaceType> = A extends []
         : 从路径获得参数<Path, xs>
       : never
     : never
-type 从路径获得方法<Path, A = InterfaceType> = A extends []
-  ? []
+export type 从路径获得方法<Path, A = InterfaceType> = A extends []
+  ? never
   : A extends [infer x, ...infer xs]
     ? 'path' extends keyof x
       ? x['path'] extends Path
@@ -54,8 +54,8 @@ type 从路径获得方法<Path, A = InterfaceType> = A extends []
         : 从路径获得方法<Path, xs>
       : never
     : never
-type 从路径获得正确返回<Path, A = InterfaceType> = A extends []
-  ? []
+export type 从路径获得正确返回<Path, A = InterfaceType> = A extends []
+  ? never
   : A extends [infer x, ...infer xs]
     ? 'path' extends keyof x
       ? x['path'] extends Path
@@ -65,8 +65,8 @@ type 从路径获得正确返回<Path, A = InterfaceType> = A extends []
         : 从路径获得正确返回<Path, xs>
       : never
     : never
-type 从路径获得错误返回<Path, A = InterfaceType> = A extends []
-  ? []
+export type 从路径获得错误返回<Path, A = InterfaceType> = A extends []
+  ? never
   : A extends [infer x, ...infer xs]
     ? 'path' extends keyof x
       ? x['path'] extends Path
@@ -75,7 +75,20 @@ type 从路径获得错误返回<Path, A = InterfaceType> = A extends []
           : never
         : 从路径获得错误返回<Path, xs>
       : never
-  : never
+    : never
+export type 从路径获得泛化的错误返回<Path, A = InterfaceType> = A extends []
+  ? never
+  : A extends [infer x, ...infer xs]
+    ? 'path' extends keyof x
+      ? x['path'] extends Path
+        ? 'errorOutput' extends keyof x
+          ? 'data' extends keyof x['errorOutput']
+            ? { status: 'fail'; data: x['errorOutput']['data'] | string }
+            : never
+          : never
+        : 从路径获得错误返回<Path, xs>
+      : never
+    : never
 
 export type 请求后端函数类型 = <路径 extends 元组转联合<所有接口路径们>>(
   路径: 路径,
