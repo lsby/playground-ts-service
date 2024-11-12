a37dbb797574d5a4cab6785532ccae509cd60ba1
hbybyyang <2451759073@qq.com>
2024-11-12 16:23:44 +0800

    统一命名

--- src/interface/interface-list.ts
+++ src/interface/interface-list.ts
@@ -3,9 +3,9 @@ import { 任意接口 } from '@lsby/net-core'
 import _src_interface_base_add_index_ts from './base/add/index'
 import _src_interface_base_sub_index_ts from './base/sub/index'
 import _src_interface_base_upload_file_index_ts from './base/upload-file/index'
-import _src_interface_user_is_login_is_login_ts from './user/is-login/is-login'
-import _src_interface_user_login_login_ts from './user/login/login'
-import _src_interface_user_register_register_ts from './user/register/register'
+import _src_interface_user_is_login_is_login_ts from './user/is-login'
+import _src_interface_user_login_login_ts from './user/login'
+import _src_interface_user_register_register_ts from './user/register'
 
 export var interfaceList: 任意接口[] = [
   _src_interface_base_add_index_ts,
