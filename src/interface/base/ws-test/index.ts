aacd4a46ca0c79aa011fd829e647751b99303ae2
hbybyyang <2451759073@qq.com>
2024-11-23 03:26:23 +0800

    整理

--- src/interface/base/ws-test/index.ts
+++ src/interface/base/ws-test/index.ts
@@ -1,5 +1,5 @@
 import { JSON状态接口 } from '@lsby/net-core'
-import { WebSocket测试 } from '../../../interface-action/base/ws'
+import { WebSocket测试 } from '../../../interface-action/base/ws-test'
 import 接口描述 from './type'
 
 export default new JSON状态接口<typeof 接口描述>(接口描述, new WebSocket测试())
