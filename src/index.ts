7dc896e680f0fef4b6b0bb10ded4c1dd460e5781
hbybyyang <2451759073@qq.com>
2024-11-25 16:45:39 +0800

    初始化脚本模板

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 import { App } from './app/app'
 
+// var kysely = await Global.getItem('kysely')
+// await kysely
+//   .获得句柄()
+//   .insertInto('user')
+//   .values({
+//     id: randomUUID(),
+//     name: 'admin',
+//     pwd: '123456',
+//   })
+//   .execute()
+
 new App().run().catch(console.error)
