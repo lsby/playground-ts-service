5b058c7f3feb43f823d12fff240490aedbf5bcec
hbybyyang <2451759073@qq.com>
2025-02-11 02:17:54 +0800

    更新依赖

--- src/types/interface-table-type.ts
+++ src/types/interface-table-type.ts
@@ -1,4 +1,4 @@
-export type InterfaceTableType = [{ 路径: "/table/user-info"; 构造参数: {}; 增参数_数据们: ({ id?: string | undefined; } & { name?: string | undefined; })[]; 删参数_筛选条件: 条件组<{ id: string; name: string; }>; 改参数_新值: Partial<{ id: string; name: string; }>; 改参数_筛选条件: 条件组<{ id: string; name: string; }>; 查参数_筛选条件: 条件组<{ id: string; name: string; }>; 查参数_分页条件: 分页选项; 查参数_排序条件: 排序选项<"id" | "name">; 增原始错误值: "不允许"; 删原始错误值: "未登录"; 改原始错误值: "未登录"; 查原始错误值: "未登录"; 增原始正确值: {}; 删原始正确值: {}; 改原始正确值: {}; 查原始正确值: { id: string; name: string; }[]; 增包装结果: { status: "fail"; data: "不允许"; } | { status: "success"; data: {}; }; 删包装结果: { status: "fail"; data: "未登录"; } | { status: "success"; data: {}; }; 改包装结果: { status: "fail"; data: "未登录"; } | { status: "success"; data: {}; }; 查包装结果: { status: "fail"; data: "未登录"; } | { status: "success"; data: { id: string; name: string; }[]; }; }]
+export type InterfaceTableType = [{ 路径: "/table/user-info"; 构造参数: {}; 增参数_数据们: ({ id?: string | undefined; } & { name?: string | undefined; })[]; 删参数_筛选条件: 条件组<{ id: string; name: string; }>; 改参数_新值: Partial<{ id: string; } & { name: string; }>; 改参数_筛选条件: 条件组<{ id: string; name: string; }>; 查参数_筛选条件: 条件组<{ id: string; name: string; }>; 查参数_分页条件: 分页选项; 查参数_排序条件: 排序选项<"id" | "name">; 增原始错误值: "不允许"; 删原始错误值: "未登录"; 改原始错误值: "未登录"; 查原始错误值: "未登录"; 增原始正确值: {}; 删原始正确值: {}; 改原始正确值: {}; 查原始正确值: { id: string; name: string; }[]; 增包装结果: { status: "fail"; data: "不允许"; } | { status: "success"; data: {}; }; 删包装结果: { status: "fail"; data: "未登录"; } | { status: "success"; data: {}; }; 改包装结果: { status: "fail"; data: "未登录"; } | { status: "success"; data: {}; }; 查包装结果: { status: "fail"; data: "未登录"; } | { status: "success"; data: { id: string; name: string; }[]; }; }]
 
 export type 是any<T> = 0 extends 1 & T ? true : false
 export type 条件<列定义> =
@@ -42,7 +42,7 @@ export type 翻译查询列描述<对象> =
           ? '可空' extends keyof 对象[key]
             ? 对象[key]['可空'] extends 'false'
               ? 翻译自定义类型<对象[key]['类型']>
-              : 翻译自定义类型<对象[key]['类型']> | undefined
+              : 翻译自定义类型<对象[key]['类型']> | null
             : never
           : never
       }
@@ -57,6 +57,22 @@ export type 翻译插入列描述<对象> =
                 ? 对象[key]['可选'] extends 'false'
                   ? 翻译自定义类型<对象[key]['类型']>
                   : 翻译自定义类型<对象[key]['类型']> | undefined
+                : never
+              : never
+          }>
+        >
+      >
+export type 翻译修改值列描述<对象> =
+  是any<对象> extends true
+    ? any
+    : 归约数组对象<
+        联合转元组<
+          未定义对象转可选对象<{
+            [key in keyof 对象]: '类型' extends keyof 对象[key]
+              ? '可空' extends keyof 对象[key]
+                ? 对象[key]['可空'] extends 'false'
+                  ? 翻译自定义类型<对象[key]['类型']>
+                  : 翻译自定义类型<对象[key]['类型']> | null
                 : never
               : never
           }>
