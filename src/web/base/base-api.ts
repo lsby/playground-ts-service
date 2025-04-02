9c002ee3d42146ce41a5871e45d614f9b68ee0d3
hbybyyang <2451759073@qq.com>
2025-04-03 02:50:58 +0800

    请求使用扩展的post

--- src/web/base/base-api.ts
+++ src/web/base/base-api.ts
@@ -1,3 +1,4 @@
+import { 不安全的扩展WebPost } from '@lsby/ts-post-extend'
 import { 组件基类 } from './base'
 
 export abstract class API组件基类<
@@ -5,10 +6,11 @@ export abstract class API组件基类<
     string,
     {
       path: string
-      method: 'get' | 'post'
+      method: 'post'
       input: Record<string, any>
       errorOutput: { status: 'fail'; data: string }
       successOutput: { status: 'success'; data: Record<string, any> }
+      webSocketData?: Record<string, any>
     }
   >,
   属性类型 extends Record<string, string>,
@@ -17,13 +19,12 @@ export abstract class API组件基类<
 > extends 组件基类<属性类型, 发出事件类型, 监听事件类型> {
   protected async 请求接口<接口名称 extends keyof 接口定义>(
     接口名称: 接口名称,
-    方法: 接口定义[接口名称]['method'],
     参数: 接口定义[接口名称]['input'],
+    头?: object,
+    ws信息回调?: (data: 接口定义[接口名称]['webSocketData']) => Promise<void>,
+    ws关闭回调?: (e: CloseEvent) => Promise<void>,
+    ws错误回调?: (e: Event) => Promise<void>,
   ): Promise<接口定义[接口名称]['errorOutput'] | 接口定义[接口名称]['successOutput']> {
-    return await fetch(this.获得属性(接口名称.toString()), {
-      method: 方法,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(参数),
-    }).then((response) => response.json())
+    return 不安全的扩展WebPost(this.获得属性(接口名称.toString()), 参数, 头, ws信息回调, ws关闭回调, ws错误回调)
   }
 }
