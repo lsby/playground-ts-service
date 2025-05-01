ae7625ad7b5dbc66a629cd3f431f00a60aeb7d30
hbybyyang <2451759073@qq.com>
2025-05-01 11:40:25 +0800

    整理

--- src/interface/interface-list.ts
+++ src/interface/interface-list.ts
@@ -1,7 +1,6 @@
 import { 任意接口 } from '@lsby/net-core'
 
 import _src_interface_base_add_index_ts from './base/add/index'
-import _src_interface_base_mul_index_ts from './base/mul/index'
 import _src_interface_base_sub_index_ts from './base/sub/index'
 import _src_interface_base_upload_file_index_ts from './base/upload-file/index'
 import _src_interface_base_ws_test_index_ts from './base/ws-test/index'
@@ -11,7 +10,6 @@ import _src_interface_user_register_index_ts from './user/register/index'
 
 export let interfaceApiList: 任意接口[] = [
   _src_interface_base_add_index_ts,
-  _src_interface_base_mul_index_ts,
   _src_interface_base_sub_index_ts,
   _src_interface_base_upload_file_index_ts,
   _src_interface_base_ws_test_index_ts,
