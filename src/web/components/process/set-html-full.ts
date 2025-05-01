ffc3e9aa5af8359e5eb6704145f1c07b4ad9cfc1
hbybyyang <2451759073@qq.com>
2025-05-02 01:19:26 +0800

    整理

---
+++ src/web/components/process/set-html-full.ts
@@ -0,0 +1,22 @@
+import { 组件基类 } from '../../base/base'
+
+type 属性类型 = {}
+type 发出事件类型 = {}
+type 监听事件类型 = {}
+
+export class LsbySetHtmlFull extends 组件基类<属性类型, 发出事件类型, 监听事件类型> {
+  static override 观察的属性: Array<keyof 属性类型> = []
+  static {
+    this.注册组件('lsby-set-html-full', this)
+  }
+
+  constructor(属性: 属性类型) {
+    super(属性)
+  }
+
+  protected override async 当加载时(): Promise<void> {
+    document.body.style.margin = '0px'
+    document.body.style.height = '100%'
+    document.documentElement.style.height = '100%'
+  }
+}
--- src/web/process/set-html-full.ts
+++
@@ -1,3 +0,0 @@
-document.body.style.margin = '0px'
-document.body.style.height = '100%'
-document.documentElement.style.height = '100%'
