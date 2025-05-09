82f3152c9587441e169f8d0912274c87283d23ff
hbybyyang <2451759073@qq.com>
2025-05-09 13:27:05 +0800

    支持mysql

---
+++ src/global/db-dialect.ts
@@ -0,0 +1,32 @@
+import SQLite from 'better-sqlite3'
+import { SqliteDialect } from 'kysely'
+import { env } from './global'
+
+export let 创建sqlite数据库适配器 = async (): Promise<SqliteDialect> => {
+  let e = await env.获得环境变量()
+  return new SqliteDialect({ database: new SQLite(e.DATABASE_PATH) })
+}
+// export let 创建pg数据库适配器 = async (): Promise<MysqlDialect> => {
+//   let e = await env.获得环境变量()
+//   return new MysqlDialect({
+//     pool: mysql2.createPool({
+//       host: e.DB_HOST,
+//       port: e.DB_PORT,
+//       user: e.DB_USER,
+//       password: e.DB_PWD,
+//       database: e.DB_NAME,
+//     }),
+//   })
+// }
+// export let 创建mysql数据库适配器 = async (): Promise<MysqlDialect> => {
+//   let e = await env.获得环境变量()
+//   return new MysqlDialect({
+//     pool: mysql2.createPool({
+//       host: e.DB_HOST,
+//       port: e.DB_PORT,
+//       user: e.DB_USER,
+//       password: e.DB_PWD,
+//       database: e.DB_NAME,
+//     }),
+//   })
+// }
--- src/global/global.ts
+++ src/global/global.ts
@@ -5,22 +5,31 @@ import { Env } from '@lsby/ts-env'
 import { GlobalAsyncItem, GlobalItem, GlobalService } from '@lsby/ts-global'
 import { Kysely管理器 } from '@lsby/ts-kysely'
 import { Log } from '@lsby/ts-log'
-import SQLite from 'better-sqlite3'
-import { SqliteDialect } from 'kysely'
 import { z } from 'zod'
 import { DB } from '../types/db'
+import { 创建sqlite数据库适配器 } from './db-dialect'
 
 export let CONST = {
   INIT_FLAG: 'INIT_FLAG',
 }
 
-let env = new Env({
+export let env = new Env({
   环境变量名称: 'ENV_FILE_PATH',
   环境描述: z.object({
     NODE_ENV: z.enum(['development', 'production', 'test']),
     DEBUG_NAME: z.string(),
-    DB_TYPE: z.enum(['sqlite', 'pg']),
+    // DB START
+    DB_TYPE: z.enum(['sqlite', 'pg', 'mysql']),
+    // pg/mysql
+    // DB_USER: z.string(),
+    // DB_PWD: z.string(),
+    // DB_HOST: z.string(),
+    // DB_PORT: z.coerce.number(),
+    // DB_NAME: z.string(),
+    // SHADOW_DB_NAME: z.string(),
+    // sqlite
     DATABASE_PATH: z.string(),
+    // DB END
     APP_PORT: z.coerce.number(),
     SYSTEM_USER: z.string(),
     SYSTEM_PWD: z.string(),
@@ -42,18 +51,20 @@ export let Global = new GlobalService([
     return new Log(e.DEBUG_NAME)
   }),
   new GlobalAsyncItem('kysely', async () => {
-    let e = await env.获得环境变量()
-    return new Kysely管理器<DB>(new SqliteDialect({ database: new SQLite(e.DATABASE_PATH) }))
+    return new Kysely管理器<DB>(await 创建sqlite数据库适配器())
+    // return new Kysely管理器<DB>(await 创建pg数据库适配器())
+    // return new Kysely管理器<DB>(await 创建mysql数据库适配器())
   }),
   new GlobalAsyncItem('jwt-plugin', async () => {
     let e = await env.获得环境变量()
     return new JWT插件(z.object({ userId: z.string().or(z.undefined()) }), e.JWT_SECRET, e.JWT_EXPIRES_IN)
   }),
   new GlobalAsyncItem('kysely-plugin', async () => {
-    let e = await env.获得环境变量()
     return new Kysely插件(
       'kysely',
-      new Kysely管理器<DB>(new SqliteDialect({ database: new SQLite(e.DATABASE_PATH) })).获得句柄(),
+      new Kysely管理器<DB>(await 创建sqlite数据库适配器()).获得句柄(),
+      // new Kysely管理器<DB>(await 创建pg数据库适配器()).获得句柄(),
+      // new Kysely管理器<DB>(await 创建mysql数据库适配器()).获得句柄(),
     )
   }),
 ])
