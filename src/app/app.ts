3d31f0eafdd6b10053227a7ead9ce0d661483d31
hbybyyang <2451759073@qq.com>
2025-05-18 23:34:54 +0800

    修复静态文件路径

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,11 +13,7 @@ export class App {
     await cron.执行([onTimeAlarm])
 
     let 静态文件目录: string
-    if (env.NODE_ENV === 'production') {
-      静态文件目录 = resolve(import.meta.dirname, '../../web')
-    } else {
-      静态文件目录 = resolve(import.meta.dirname, '../../dist/web')
-    }
+    静态文件目录 = resolve(import.meta.dirname, '../../web')
     let service = new 服务器(interfaceApiList, env.APP_PORT, 静态文件目录)
     let serviceInfo = await service.run()
 
