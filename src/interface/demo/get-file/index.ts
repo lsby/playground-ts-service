1584f2bbf9600127667523bd6a4bc689e3dc2777
hbybyyang <2451759073@qq.com>
2025-05-26 03:02:08 +0800

    增加返回文件示例

---
+++ src/interface/demo/get-file/index.ts
@@ -0,0 +1,47 @@
+import {
+  JSON解析插件,
+  去除只读,
+  发送文件插件,
+  合并插件结果,
+  延时直接形式转换器,
+  接口,
+  接口逻辑,
+  获得接口逻辑正确类型,
+  获得接口逻辑错误类型,
+} from '@lsby/net-core'
+import { Either, Right, Task } from '@lsby/ts-fp-data'
+import { z } from 'zod'
+import { Global } from '../../../global/global'
+
+let 接口路径 = '/api/demo/get-file' as const
+let 接口方法 = 'get' as const
+
+let 插件 = [new Task(async () => new 发送文件插件()), new Task(async () => new JSON解析插件(z.object({}), {}))] as const
+
+let 逻辑错误类型Zod = z.never()
+
+type 附加参数类型 = {}
+class 逻辑实现 extends 接口逻辑<插件类型, 附加参数类型, 逻辑错误类型, 逻辑正确类型> {
+  override 获得插件们(): 插件类型 {
+    return [...插件]
+  }
+
+  override async 实现(参数: 参数类型, _附加参数: 附加参数类型): Promise<Either<逻辑错误类型, 逻辑正确类型>> {
+    let _log = (await Global.getItem('log')).extend(接口路径)
+    return new Right({ fn: () => 参数.sendFile(new Buffer('aaa')) })
+  }
+}
+let 接口实现 = 接口逻辑.混合([new 逻辑实现()])
+
+type 插件类型 = 去除只读<typeof 插件>
+type 参数类型 = 合并插件结果<插件类型>
+type 逻辑错误类型 = z.infer<typeof 逻辑错误类型Zod>
+type 逻辑正确类型 = { fn: () => Buffer }
+let 接口错误输出形式 = z.object({ status: z.literal('fail'), data: 逻辑错误类型Zod })
+let 接口正确输出形式 = z.instanceof(Buffer)
+let 接口转换器 = new 延时直接形式转换器<
+  Buffer,
+  获得接口逻辑错误类型<typeof 接口实现>,
+  获得接口逻辑正确类型<typeof 接口实现>
+>()
+export default new 接口(接口路径, 接口方法, 接口实现, 接口错误输出形式, 接口正确输出形式, 接口转换器)
--- src/interface/interface-list.ts
+++ src/interface/interface-list.ts
@@ -1,3 +1,4 @@
+// 该文件由脚本自动生成, 请勿修改.
 import { 任意接口 } from '@lsby/net-core'
 
 import _src_interface_demo_add_index_ts from './demo/add/index'
@@ -8,6 +9,7 @@ import _src_interface_user_is_login_index_ts from './user/is-login/index'
 import _src_interface_user_login_index_ts from './user/login/index'
 import _src_interface_user_register_index_ts from './user/register/index'
 import _src_interface_demo_get_list_index_ts from './demo/get-list/index'
+import _src_interface_demo_get_file_index_ts from './demo/get-file/index'
 
 export let interfaceApiList: 任意接口[] = [
   _src_interface_demo_add_index_ts,
@@ -18,4 +20,5 @@ export let interfaceApiList: 任意接口[] = [
   _src_interface_user_login_index_ts,
   _src_interface_user_register_index_ts,
   _src_interface_demo_get_list_index_ts,
+  _src_interface_demo_get_file_index_ts,
 ]
