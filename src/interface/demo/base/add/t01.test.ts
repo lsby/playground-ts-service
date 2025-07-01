394baa9542d87f58082cb877d46346aef5b92ca3
hbybyyang <2451759073@qq.com>
2025-07-02 07:43:15 +0800

    整理

--- src/interface/demo/base/add/t01.test.ts
+++ src/interface/demo/base/add/t01.test.ts
@@ -1,6 +1,5 @@
 import { 接口测试 } from '@lsby/net-core'
 import assert from 'assert'
-import { Global } from '../../../../global/global'
 import { 请求用例 } from '../../../../tools/request'
 import 接口 from './index'
 
@@ -15,15 +14,25 @@ export default new 接口测试(
   },
 
   async (中置结果: object): Promise<void> => {
-    let log = await Global.getItem('log')
-    let 错误结果 = 接口.获得接口错误形式Zod().safeParse(中置结果)
-    let 正确结果 = 接口.获得接口正确形式Zod().safeParse(中置结果)
-    if (错误结果.success === false && 正确结果.success === false) {
-      await log.error('没有通过返回值检查: %o, %o', 正确结果.error.errors, 错误结果.error.errors)
-      throw new Error('非预期的返回值')
+    console.log('实际结果: %o', 中置结果)
+
+    let 预期: string = '成功'
+
+    let 失败结果校验 = 接口.获得接口错误形式Zod().safeParse(中置结果)
+    let 正确结果校验 = 接口.获得接口正确形式Zod().safeParse(中置结果)
+
+    if (失败结果校验.success === false && 正确结果校验.success === false) {
+      throw new Error('没有通过返回值检查')
     }
-    if (正确结果.success === false) throw new Error('应该调用成功, 实际调用出错')
-    let 结果 = 正确结果.data
-    assert.equal(结果.data.res, 3)
+    if (正确结果校验.success === true) {
+      console.log('预期: %o, 实际: %o', 预期, '调用成功')
+      if (预期 === '失败') throw new Error('应该调用失败, 实际调用成功')
+    }
+    if (失败结果校验.success === true) {
+      console.log('预期: %o, 实际: %o', 预期, '调用失败')
+      if (预期 === '成功') throw new Error('应该调用成功, 实际调用出错')
+    }
+
+    assert.equal(正确结果校验.data?.data.res, 3)
   },
 )
--- src/interface/user/login/t03.test.ts
+++ src/interface/user/login/t03.test.ts
@@ -23,18 +23,25 @@ export default new 接口测试(
   },
 
   async (中置结果: object): Promise<void> => {
-    let log = await Global.getItem('log')
+    console.log('实际结果: %o', 中置结果)
 
-    let 错误结果 = 接口.获得接口错误形式Zod().safeParse(中置结果)
-    let 正确结果 = 接口.获得接口正确形式Zod().safeParse(中置结果)
-    if (错误结果.success === false && 正确结果.success === false) {
-      await log.error('没有通过返回值检查: %o, %o', 正确结果.error.errors, 错误结果.error.errors)
-      throw new Error('非预期的返回值')
+    let 预期: string = '失败'
+
+    let 失败结果校验 = 接口.获得接口错误形式Zod().safeParse(中置结果)
+    let 正确结果校验 = 接口.获得接口正确形式Zod().safeParse(中置结果)
+
+    if (失败结果校验.success === false && 正确结果校验.success === false) {
+      throw new Error('没有通过返回值检查')
+    }
+    if (正确结果校验.success === true) {
+      console.log('预期: %o, 实际: %o', 预期, '调用成功')
+      if (预期 === '失败') throw new Error('应该调用失败, 实际调用成功')
+    }
+    if (失败结果校验.success === true) {
+      console.log('预期: %o, 实际: %o', 预期, '调用失败')
+      if (预期 === '成功') throw new Error('应该调用成功, 实际调用出错')
     }
 
-    if (错误结果.success === false) throw new Error('应该调用出错, 实际调用成功')
-    let 结果 = 错误结果.data
-
-    assert.equal(结果.data, '用户不存在或密码错误')
+    assert.equal(失败结果校验.data?.data, '用户不存在或密码错误')
   },
 )
