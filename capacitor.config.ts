de0014ed42a93b546abc6932b4a575f512934bed
hbybyyang <2451759073@qq.com>
2025-08-07 06:51:48 +0800

    整理

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'lsby.playground.ts.service',
-  appName: '@lsby/playground-ts-service',
-  webDir: 'dist/web',
-}
-
-export default config
