44197f8f6ba6b1b129fb0da857df651773c998c5
hbybyyang <2451759073@qq.com>
2025-08-07 06:04:40 +0800

    支持编译到安卓

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'lsby.playground.ts.service',
+  appName: '@lsby/playground-ts-service',
+  webDir: 'dist/web',
+}
+
+export default config
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,12 @@
 import * as lsby from '@lsby/eslint-config'
 
-export default lsby.推荐配置
+var 常用忽略域 = [...lsby.常用忽略域, 'android']
+var 忽略常见文件夹 = { ignores: 常用忽略域 }
+
+export default [
+  // ..
+  忽略常见文件夹,
+  lsby.ts安全性,
+  lsby.jsDoc安全性,
+  lsby.风格美化,
+]
