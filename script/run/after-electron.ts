14488fd81449dda29d140690391c47ebbdadafa9
hbybyyang <2451759073@qq.com>
2025-09-13 06:32:24 +0800

    整理

--- script/run/after-electron.ts
+++ script/run/after-electron.ts
@@ -20,12 +20,6 @@ function 执行后处理(生成目录: string): void {
   fs.copyFileSync(envSource, env目标)
   console.log(`✅ 已复制 ${envSource} 到 ${env目标}`)
 
-  const runCmd内容 = ['chcp 65001', 'set DEBUG=*', 'set ENV_FILE_PATH=.env'].join('\r\n')
-
-  const runCmd路径 = path.join(生成目录, 'run.cmd')
-  fs.writeFileSync(runCmd路径, runCmd内容, { encoding: 'utf8' })
-  console.log(`✅ 已生成 ${runCmd路径}`)
-
   const dbSource = path.resolve('db', 'prod.db')
   if (!fs.existsSync(dbSource)) {
     throw new Error('❌ 未找到 db/prod.db 文件，无法继续。')
@@ -36,6 +30,12 @@ function 执行后处理(生成目录: string): void {
   const db目标 = path.join(db目标目录, 'prod.db')
   fs.copyFileSync(dbSource, db目标)
   console.log(`✅ 已复制 ${dbSource} 到 ${db目标}`)
+
+  const runCmd内容 = ['chcp 65001', 'set DEBUG=*', 'set ENV_FILE_PATH=.env', 'playground-ts-service.exe'].join('\r\n')
+
+  const runCmd路径 = path.join(生成目录, 'run.cmd')
+  fs.writeFileSync(runCmd路径, runCmd内容, { encoding: 'utf8' })
+  console.log(`✅ 已生成 ${runCmd路径}`)
 }
 
 执行后处理(path.resolve('package', 'win-unpacked'))
