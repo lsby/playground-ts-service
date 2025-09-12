b74a74b1df518cee32e618f50598ccf9c00a2e28
hbybyyang <2451759073@qq.com>
2025-09-13 04:44:39 +0800

    整理

--- src/electron.ts
+++ src/electron.ts
@@ -2,9 +2,12 @@ import { app, BrowserWindow } from 'electron'
 import { App } from './app/app'
 import { Global } from './global/global'
 
-export let 主窗口: BrowserWindow | null = null
+await new App().run()
 
+export let 主窗口: BrowserWindow | null = null
 async function 创建主窗口(): Promise<void> {
+  let env = await Global.getItem('env').then((a) => a.获得环境变量())
+
   主窗口 = new BrowserWindow({
     width: 800,
     height: 600,
@@ -13,16 +16,11 @@ async function 创建主窗口(): Promise<void> {
       contextIsolation: true,
     },
   })
-
   主窗口.webContents.openDevTools()
 
-  let env = await Global.getItem('env').then((a) => a.获得环境变量())
   await 主窗口.loadURL(`http://127.0.0.1:${env.WEB_PORT}/`)
-
   主窗口.on('closed', () => (主窗口 = null))
 }
-
-await new App().run()
 
 app.on('ready', 创建主窗口)
 app.on('window-all-closed', () => {
