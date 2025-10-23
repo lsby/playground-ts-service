d01fe1b5f6eb07c368465f989319d977cda4ad88
hbybyyang <2451759073@qq.com>
2025-10-24 00:36:02 +0800

    整理

--- script/gen/write-version.ts
+++ script/gen/write-version.ts
@@ -5,4 +5,4 @@ const 包信息 = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
 const 版本号 = 包信息.version
 
 const 输出路径 = path.resolve(import.meta.dirname, '../../src/app/version.ts')
-fs.writeFileSync(输出路径, `export let version: string = "${版本号}"\n`)
+fs.writeFileSync(输出路径, `export let version: string = '${版本号}'\n`)
