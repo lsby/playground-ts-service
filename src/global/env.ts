4ff417e9f105a4c4181965fb68826562e30cafa2
hbybyyang <2451759073@qq.com>
2025-11-14 22:43:08 +0800

    整理

--- src/global/env.ts
+++ src/global/env.ts
@@ -4,9 +4,11 @@ import { z } from 'zod'
 export let 环境变量 = await new Env({
   环境变量名称: 'ENV_FILE_PATH',
   环境描述: z.object({
+    // 环境名称
     NODE_ENV: z.enum(['development', 'production', 'test']),
+    // 调试名称
     DEBUG_NAME: z.string(),
-    // DB START
+    // 数据库部分 开始
     DB_TYPE: z.enum(['sqlite', 'pg', 'mysql']),
     // sqlite
     DB_PATH: z.string(),
@@ -21,12 +23,16 @@ export let 环境变量 = await new Env({
     // DB_PORT: z.coerce.number(),
     // DB_NAME: z.string(),
     // SHADOW_DB_NAME: z.string(),
-    // DB END
+    // 数据库部分 结束
+    // 应用端口
     APP_PORT: z.coerce.number(),
     WEB_PORT: z.coerce.number(),
+    // 系统用户
     SYSTEM_USER: z.string(),
     SYSTEM_PWD: z.string(),
+    // 文件上传
     UPLOAD_MAX_FILE_SIZE: z.coerce.number(),
+    // JWT
     JWT_SECRET: z.string(),
     JWT_EXPIRES_IN: z.string(),
   }),
