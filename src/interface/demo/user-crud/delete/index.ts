48362464bb5769cbe73fd8bd0d15842982844cfb
lsby <2451759073@qq.com>
2025-11-26 06:53:38 +0800

    修复用户删除接口

--- src/interface/demo/user-crud/delete/index.ts
+++ src/interface/demo/user-crud/delete/index.ts
@@ -1,4 +1,5 @@
 import { 常用形式接口封装, 接口逻辑 } from '@lsby/net-core'
+import { Kysely管理器 } from '@lsby/ts-kysely'
 import { z } from 'zod'
 import { jwt插件, kysely插件 } from '../../../../global/plugin'
 import { 检查JSON参数 } from '../../../../interface-logic/check/check-json-args'
@@ -19,9 +20,25 @@ let 接口逻辑实现 = 接口逻辑
   )
   .混合(new 检查JSON参数(z.object({ id: z.string() })))
   .混合(
-    new 删除逻辑(kysely插件, 'user', async (data) => ({
-      条件们: [['id', '=', data.id]],
-    })),
+    接口逻辑.构造([kysely插件], async (参数, 逻辑附加参数, 请求附加参数) => {
+      let _log = 请求附加参数.log.extend(删除逻辑.name)
+
+      return 参数.kysely.执行事务Either(async (trx) => {
+        return 接口逻辑
+          .空逻辑()
+          .混合(
+            new 删除逻辑(kysely插件, 'user_config', async () => ({
+              条件们: [['user_id', '=', 逻辑附加参数.id]],
+            })),
+          )
+          .混合(
+            new 删除逻辑(kysely插件, 'user', async () => ({
+              条件们: [['id', '=', 逻辑附加参数.id]],
+            })),
+          )
+          .实现({ kysely: Kysely管理器.从句柄创建(trx) }, {}, 请求附加参数)
+      })
+    }),
   )
 
 let 接口错误类型描述 = z.enum(['未登录', '非管理员'])
