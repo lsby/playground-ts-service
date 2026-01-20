e261ecd14d15675f944af0b3e42b04fc2dbaa537
lsby <2451759073@qq.com>
2026-01-20 15:02:12 +0800

    增加自定义eslint规则框架

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import * as eslintConfig from '@lsby/eslint-config'
+import projectRules from './script/eslint/project-rules.js'
 
 var 常用忽略域 = [...eslintConfig.常用忽略域, 'android', 'package']
 var 忽略常见文件夹 = { ignores: 常用忽略域 }
@@ -9,4 +10,11 @@ export default [
   eslintConfig.ts安全性,
   eslintConfig.jsDoc安全性,
   eslintConfig.风格美化,
+  {
+    plugins: { project: projectRules },
+    rules: {
+      // ..
+      'project/no-dom-query': 'error',
+    },
+  },
 ]
